65e3706d65fc6eac0ff8e64ae8bd5eaa34456dfc
David Morgan <david.morgan@europe.com>
2022-04-15 13:53:20 +0100

    Init test env variables in setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Setup test environment variables
+process.env.REACT_APP_ECOM_API_URL = 'http://localhost:4001/';
