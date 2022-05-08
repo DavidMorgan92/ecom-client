4e5dff98a94ee30aa0cbbf64efc58817bc87fdf1
David Morgan <david.morgan@europe.com>
2022-05-08 22:40:50 +0100

    Cart link and size shown in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
 	logout,
 	selectAuthenticated,
 	selectEmail,
 } from '../../store/authSlice';
+import { getCart, selectCart } from '../../store/cartSlice';
 import Search from '../search';
 
 /**
@@ -17,8 +19,18 @@ export default function Header() {
 	const authenticated = useSelector(selectAuthenticated);
 	const email = useSelector(selectEmail);
 
+	// Get information about the cart
+	const cart = useSelector(selectCart);
+
 	// Use dispatch to communicate with auth redux store
 	const dispatch = useDispatch();
+
+	// Get cart information if user is authenticated
+	useEffect(() => {
+		if (authenticated) {
+			dispatch(getCart());
+		}
+	}, [dispatch, authenticated]);
 
 	// Handle logout button click
 	function handleLogout() {
@@ -40,6 +52,9 @@ export default function Header() {
 	// Content to show when the user is authenticated
 	const authenticatedGroup = (
 		<>
+			{/* Cart link */}
+			<Link to='/cart' data-testid='cart-link'>Cart ({cart.length})</Link>
+
 			{/* Show the authenticated user's email */}
 			<Link to='/account' data-testid='account-link'>
 				{email}
--- src/components/header/index.test.js
+++ src/components/header/index.test.js
@@ -5,6 +5,7 @@ import { MemoryRouter } from 'react-router-dom';
 import Header from '.';
 import store from '../../store';
 import authSlice from '../../store/authSlice';
+import * as cartSlice from '../../store/cartSlice';
 import productsSlice from '../../store/productsSlice';
 
 describe('Header component', () => {
@@ -25,6 +26,7 @@ describe('Header component', () => {
 		expect(screen.getByText('Register')).toHaveAttribute('href', '/register');
 
 		// Expect authenticated link group not to be shown
+		expect(screen.queryByTestId('cart-link')).not.toBeInTheDocument();
 		expect(screen.queryByTestId('account-link')).not.toBeInTheDocument();
 		expect(screen.queryByText('Logout')).not.toBeInTheDocument();
 	});
@@ -35,6 +37,7 @@ describe('Header component', () => {
 			reducer: {
 				auth: authSlice,
 				products: productsSlice,
+				cart: cartSlice.default,
 			},
 			preloadedState: {
 				auth: {
@@ -60,6 +63,7 @@ describe('Header component', () => {
 		expect(screen.queryByText('Register')).not.toBeInTheDocument();
 
 		// Expect authenticated link group to be shown
+		expect(screen.getByTestId('cart-link')).toBeInTheDocument();
 		expect(screen.getByTestId('account-link')).toBeInTheDocument();
 		expect(screen.getByText('Logout')).toBeInTheDocument();
 
@@ -75,6 +79,7 @@ describe('Header component', () => {
 			reducer: {
 				auth: authSlice,
 				products: productsSlice,
+				cart: cartSlice.default,
 			},
 			preloadedState: {
 				auth: {
@@ -105,5 +110,70 @@ describe('Header component', () => {
 		// Expect authenticated link group not to be shown
 		expect(screen.queryByTestId('account-link')).not.toBeInTheDocument();
 		expect(screen.queryByText('Logout')).not.toBeInTheDocument();
+	});
+
+	it('shows correct cart item count in link group', () => {
+		// Create mock auth store for authenticated user with 2 cart items
+		const mockStore = configureStore({
+			reducer: {
+				auth: authSlice,
+				products: productsSlice,
+				cart: cartSlice.default,
+			},
+			preloadedState: {
+				auth: {
+					authenticated: true,
+					email: 'david.morgan@gmail.com',
+				},
+				cart: {
+					cart: [{}, {}],
+				},
+			},
+		});
+
+		// Render component
+		render(
+			<Provider store={mockStore}>
+				<Header />
+			</Provider>,
+			{ wrapper: MemoryRouter },
+		);
+
+		// Expect 2 cart items to be shown in the link text
+		expect(screen.getByTestId('cart-link')).toHaveTextContent('Cart (2)');
+	});
+
+	it('dispatches getCart thunk on mount', () => {
+		// Spy on getCart thunk
+		const getCartSpy = jest.spyOn(cartSlice, 'getCart');
+
+		// Create mock auth store for authenticated user
+		const mockStore = configureStore({
+			reducer: {
+				auth: authSlice,
+				products: productsSlice,
+				cart: cartSlice.default,
+			},
+			preloadedState: {
+				auth: {
+					authenticated: true,
+					email: 'david.morgan@gmail.com',
+				},
+			},
+		});
+
+		// Render component
+		render(
+			<Provider store={mockStore}>
+				<Header />
+			</Provider>,
+			{ wrapper: MemoryRouter },
+		);
+
+		// Expect the getCart thunk to have been called once
+		expect(getCartSpy).toHaveBeenCalledTimes(1);
+
+		// Restore the getCart implementation
+		getCartSpy.mockRestore();
 	});
 });
