33b82e36a8157486601d9cb86d8eaf0bc8b9839b
David Morgan <david.morgan@europe.com>
2022-05-17 18:13:42 +0100

    Add product to cart

--- src/routes/productDetails/index.js
+++ src/routes/productDetails/index.js
@@ -1,5 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { selectAuthenticated } from '../../store/authSlice';
+import {
+	selectCart,
+	selectUpdateCartFailed,
+	selectUpdateCartPending,
+	updateCart,
+} from '../../store/cartSlice';
 import {
 	getProductById,
 	selectGetProductByIdFailed,
@@ -24,6 +31,17 @@ export default function ProductDetails() {
 	const products = useSelector(selectProducts);
 	const getProductByIdFailed = useSelector(selectGetProductByIdFailed);
 
+	// Get the current cart from the cart redux store for adding
+	const cart = useSelector(selectCart);
+	const updateCartPending = useSelector(selectUpdateCartPending);
+	const updateCartFailed = useSelector(selectUpdateCartFailed);
+
+	// Get authenticated state from auth redux store
+	const authenticated = useSelector(selectAuthenticated);
+
+	// Use navigate to go to login page if not authenticated
+	const navigate = useNavigate();
+
 	// Get the product with the matching ID
 	const product = products.find(p => p.id === Number(productId));
 
@@ -41,11 +59,37 @@ export default function ProductDetails() {
 		return <div>Loading product information...</div>;
 	}
 
+	// User clicked the add to cart button
+	function handleAddToCart() {
+		// If not authenticated, redirect to login page with redirect back to this product page
+		if (!authenticated) {
+			navigate(`/login?redirect=/product/${productId}`);
+			return;
+		}
+
+		const newCart = [...cart, { count: 1, product }];
+
+		// Dispatch update cart thunk to add new item
+		dispatch(updateCart(newCart));
+	}
+
 	return (
 		<>
 			<div>{product.name}</div>
 			<div>{product.description}</div>
 			<div>{formatPrice(product.pricePennies)}</div>
+			<div>
+				{/* Add to cart button, disabled when update cart is pending */}
+				<button onClick={handleAddToCart} disabled={updateCartPending}>
+					Add to Cart
+				</button>
+
+				{/* Show when update cart is pending */}
+				{updateCartPending && <p>Adding item to cart...</p>}
+
+				{/* Show when update cart failed */}
+				{updateCartFailed && <p>Failed to add item to cart</p>}
+			</div>
 		</>
 	);
 }
--- src/routes/productDetails/index.test.js
+++ src/routes/productDetails/index.test.js
@@ -1,4 +1,5 @@
 import {
+	fireEvent,
 	render,
 	screen,
 	waitFor,
@@ -8,11 +9,15 @@ import { Provider } from 'react-redux';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { configureStore } from '@reduxjs/toolkit';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route, Router } from 'react-router-dom';
 import ProductDetails from '.';
 import productsSlice from '../../store/productsSlice';
+import * as cartSlice from '../../store/cartSlice';
 import { formatPrice } from '../../util';
 import { routes } from '../../services/ecom/products';
+import { routes as cartRoutes } from '../../services/ecom/cart';
+import authSlice from '../../store/authSlice';
+import { createMemoryHistory } from 'history';
 
 // Mock redux products set
 const products = [
@@ -27,11 +32,20 @@ const products = [
 // Mock redux store
 const mockStore = configureStore({
 	reducer: {
+		auth: authSlice,
 		products: productsSlice,
+		cart: cartSlice.default,
 	},
 	preloadedState: {
+		auth: {
+			authenticated: true,
+			email: 'david.morgan@gmail.com',
+		},
 		products: {
 			products,
+		},
+		cart: {
+			cart: [],
 		},
 	},
 });
@@ -53,6 +67,25 @@ const server = setupServer(
 	// Mock return of a nonexisting product
 	rest.get(routes.getOne(3).href, (req, res, ctx) => {
 		return res(ctx.status(404));
+	}),
+
+	// Mock adding an item to the cart
+	rest.put(cartRoutes.cart().href, (req, res, ctx) => {
+		return res(
+			ctx.json({
+				items: [
+					{
+						count: 1,
+						product: {
+							id: 1,
+							name: 'Toothbrush',
+							description: 'Bristly',
+							pricePennies: '123',
+						},
+					},
+				],
+			}),
+		);
 	}),
 );
 
@@ -80,6 +113,17 @@ describe('ProductDetails page', () => {
 		expect(
 			screen.getByText(formatPrice(products[0].pricePennies)),
 		).toBeInTheDocument();
+
+		// Expect add to cart button to be shown
+		expect(screen.getByText('Add to Cart')).toBeInTheDocument();
+
+		// Expect none of these messages to be shown
+		expect(
+			screen.queryByText('Adding item to cart...'),
+		).not.toBeInTheDocument();
+		expect(
+			screen.queryByText('Failed to add item to cart'),
+		).not.toBeInTheDocument();
 	});
 
 	it('requests a product from the API if it does not exist in the redux store', async () => {
@@ -138,5 +182,184 @@ describe('ProductDetails page', () => {
 
 		// Expect the 404 Page Not Found of the NoMatch component to be shown
 		expect(screen.getByText('404 Page Not Found')).toBeInTheDocument();
+	});
+
+	it('dispatches updateCart thunk when add to cart is clicked', async () => {
+		// Spy on updateCart thunk
+		const updateCartSpy = jest.spyOn(cartSlice, 'updateCart');
+
+		// Render component
+		render(
+			<Provider store={mockStore}>
+				<MemoryRouter initialEntries={['/product/1']}>
+					<Routes>
+						<Route path='/product/:productId' element={<ProductDetails />} />
+					</Routes>
+				</MemoryRouter>
+			</Provider>,
+		);
+
+		// Wait for loading message to disappear
+		await waitFor(() => {
+			expect(
+				screen.queryByText('Loading product information...'),
+			).not.toBeInTheDocument();
+		});
+
+		// Click add to cart button
+		fireEvent.click(screen.getByText('Add to Cart'));
+
+		// Expect update pending message to be shown
+		expect(
+			await screen.findByText('Adding item to cart...'),
+		).toBeInTheDocument();
+
+		// Expect failed to update message not to be shown
+		expect(
+			screen.queryByText('Failed to add item to cart'),
+		).not.toBeInTheDocument();
+
+		// Expect add to cart button to be disabled
+		expect(screen.getByText('Add to Cart')).toBeDisabled();
+
+		// Wait for update pending message to disappear
+		await waitForElementToBeRemoved(screen.getByText('Adding item to cart...'));
+
+		// Expect none of these messages to be shown
+		expect(
+			screen.queryByText('Adding item to cart...'),
+		).not.toBeInTheDocument();
+		expect(
+			screen.queryByText('Failed to add item to cart'),
+		).not.toBeInTheDocument();
+
+		// Expect add to cart button to be enabled
+		expect(screen.getByText('Add to Cart')).toBeEnabled();
+
+		// Expect update cart to have been called with a list with the additional product
+		expect(updateCartSpy).toHaveBeenCalledTimes(1);
+		expect(updateCartSpy).toHaveBeenCalledWith([
+			{
+				count: 1,
+				product: {
+					id: 1,
+					name: 'Toothbrush',
+					description: 'Bristly',
+					pricePennies: '123',
+				},
+			},
+		]);
+
+		// Restore updateCart implementation
+		updateCartSpy.mockRestore();
+	});
+
+	it('shows failed to update message when add to cart fails', async () => {
+		// Cause request to fail with 500
+		server.use(
+			rest.put(cartRoutes.cart().href, (req, res, ctx) => {
+				return res(ctx.status(500));
+			}),
+		);
+
+		// Render component
+		render(
+			<Provider store={mockStore}>
+				<MemoryRouter initialEntries={['/product/1']}>
+					<Routes>
+						<Route path='/product/:productId' element={<ProductDetails />} />
+					</Routes>
+				</MemoryRouter>
+			</Provider>,
+		);
+
+		// Wait for loading message to disappear
+		await waitFor(() => {
+			expect(
+				screen.queryByText('Loading product information...'),
+			).not.toBeInTheDocument();
+		});
+
+		// Click add to cart button
+		fireEvent.click(screen.getByText('Add to Cart'));
+
+		// Expect update pending message to be shown
+		expect(
+			await screen.findByText('Adding item to cart...'),
+		).toBeInTheDocument();
+
+		// Expect failed to update message not to be shown
+		expect(
+			screen.queryByText('Failed to add item to cart'),
+		).not.toBeInTheDocument();
+
+		// Expect add to cart button to be disabled
+		expect(screen.getByText('Add to Cart')).toBeDisabled();
+
+		// Wait for update pending message to disappear
+		await waitForElementToBeRemoved(screen.getByText('Adding item to cart...'));
+
+		// Expect update failed message to be shown
+		expect(
+			screen.queryByText('Failed to add item to cart'),
+		).toBeInTheDocument();
+
+		// Expect add to cart button to be enabled
+		expect(screen.getByText('Add to Cart')).toBeEnabled();
+	});
+
+	it('navigates to login page on add to cart if user is not authenticated', async () => {
+		// Mock store for an unauthenticated user
+		const unauthenticatedStore = configureStore({
+			reducer: {
+				auth: authSlice,
+				products: productsSlice,
+				cart: cartSlice.default,
+			},
+			preloadedState: {
+				auth: {
+					authenticated: false,
+					authPending: false,
+					authFailed: false,
+					email: null,
+					registrationPending: false,
+					registrationFailed: false,
+				},
+				products: {
+					products,
+				},
+				cart: {
+					cart: [],
+				},
+			},
+		});
+
+		// Create history to track navigation
+		const history = createMemoryHistory({ initialEntries: ['/product/1'] });
+
+		// Render component
+		render(
+			<Provider store={unauthenticatedStore}>
+				<Router navigator={history} location={history.location}>
+					<Routes>
+						<Route path='/product/:productId' element={<ProductDetails />} />
+					</Routes>
+				</Router>
+			</Provider>,
+		);
+
+		// Wait for loading message to disappear
+		await waitFor(() => {
+			expect(
+				screen.queryByText('Loading product information...'),
+			).not.toBeInTheDocument();
+		});
+
+		// Click add to cart button
+		fireEvent.click(screen.getByText('Add to Cart'));
+
+		// Expect redirect to login page with redirect back to product page
+		expect(history.location.pathname).toBe('/login');
+		expect(history.location.search).toBe('?redirect=/product/1');
 	});
 });
